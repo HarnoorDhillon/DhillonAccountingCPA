8d71a5783c63dd1e8afe4539e2f2e76ded8fe1fd
Harnoor Dhillon <dhillon.h@berkeley.edu>
2023-12-27 19:01:54 -0800

    calendly and about formatting change for mobile

--- src/ContentBox.js
+++ src/ContentBox.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ContentBox.css';
-import { Link } from 'react-scroll';
 
 const ContentBox = () => {
   return (
@@ -8,9 +7,9 @@ const ContentBox = () => {
       <h1 className="content-heading">Working with you for a better financial future</h1>
       <p className="content-text">Dhillon Accounting CPA is a full-service accounting firm dedicated to maximizing your financial benefits for both Personal and Business Taxes.</p>
       <p className="offer">FOR FREE 30 MINUTE CONSULTATION</p>
-      <Link to="contact" spy={true} smooth={true} duration={500}>
-        <button className="start-button">Get Started Now</button>
-      </Link>
+      <a href="https://calendly.com/hdhillon-cpa/30-minute-meeting" target="_blank" rel="noopener noreferrer" className="start-button-link">
+        <button className="start-button">Book Appointment</button>
+      </a>
     </div>
   );
 };
